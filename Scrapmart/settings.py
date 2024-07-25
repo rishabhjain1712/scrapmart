d31e2365bedf8dea5349479c8f9d9656c4fd9136
rishabhjain1712 <rishabh201224@gmail.com>
2024-07-25 20:13:37 +0530

    prepare for deploy

--- Scrapmart/settings.py
+++ Scrapmart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "SECRET_KEY"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['scrapmart.onrender.com',
+ALLOWED_HOSTS = ['scrapmart-jgfe.onrender.com',
 '127.0.0.1',
 'localhost'
 ]
